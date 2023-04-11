e24b8b1be605b01f1dc17c3a1d6552749f3790ca
Mamokgosi <preciousmaphutha173@gmail.com>
2023-04-11 15:29:31 +0200

    first commit

---
+++ function convertPoundsintoKilograms(poun.js
@@ -0,0 +1,26 @@
+function convertPoundsintoKilograms(pound){
+    kilogram = pound / 2,205
+    ;
+return kilogram;
+}
+
+function convertInchesintoCentimetres(inches){
+   centimetres = inches / 2,54
+
+   return centimetres
+}
+
+function convertfahrenheitintoCelcius(fahrenheit){
+   celcius = (fahrenheit - 32) * 5/9;
+   return celcius
+}
+function convertfeetintoMeter(feet){
+   meter = feet / 3,281
+   return meter
+}
+
+
+console.log(convertPoundsintoKilograms(10))
+console.log(convertInchesintoCentimetres(20))
+console.log(convertfahrenheitintoCelcius(40))
+console.log(convertfeetintoMeter(10))
